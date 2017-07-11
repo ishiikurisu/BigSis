eaf4cb13117f3341a6d03b5d7453a135960f3a4e
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-11 11:37:45 -0300

    Adding decompressing procedure

--- onee.py
+++ onee.py
@@ -19,24 +19,39 @@ def compress(inlet):
     """Creates a compression script and runs it."""
     commands = [ ]
     file_parts = inlet.split('/')
+    # TODO Create necessary folder
     commands.append('cd box')
     for part in file_parts[:-1]:
         commands.append('cd ' + part)
     commands.append('tar czpvf - {0} | split -d -b 50M - tardisk'.format(file_parts[-1]))
     for _ in range(len(file_parts)):
         commands.append('cd ..')
-    commands.append('pwd')
     file_path = '/'.join(file_parts[:-1])
     commands.append('mv box/{0}/tardisk* {1}'.format(file_path, inlet))
     execute(commands)
 
 def decompress(inlet):
-    pass
+    """Creates a decompression script and runs it."""
+    commands = [ ]
+    file_parts = inlet.split('/')
+    outlet = file_parts[-1]
+    for part in file_parts:
+        commands.append('cd ' + part)
+    commands.append('cat tardisk* | tar xzpvf - ')
+    dots = ''
+    for _ in range(len(file_parts)):
+        dots += '../'
+    commands.append('mv %s %s' % (outlet, dots))
+    for _ in range(len(file_parts)):
+        commands.append('cd ..')
+    # TODO Create necessary folders
+    commands.append('mv %s box' % (outlet))
+    execute(commands)
 
 if __name__ == '__main__':
     if sys.argv[1] == 'compress':
         compress(sys.argv[2])
     elif sys.argv[1] == 'decompress':
-        raise NotImplementedError()
+        decompress(sys.argv[2])
     else:
         raise RuntimeError()
